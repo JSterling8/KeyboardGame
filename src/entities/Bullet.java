44f1573b1a742a1e281817fa568a67f23b5585ab
Jon <u1276062@unimail.hud.ac.uk>
2013-03-28 21:17:04 +0000

    All entities commented.

--- src/entities/Bullet.java
+++ src/entities/Bullet.java
@@ -2,71 +2,89 @@ package entities;
 
 import java.awt.Point;
 
+/**
+ * This class creates the blueprint for all Bullet objects.
+ * Bullets are fired at Enemies.
+ * 
+ * @author Jonathan Sterling
+ *
+ */
 public class Bullet{ 
-    private int startX = 0;
-    private int startY = 0;
-    private float destX = 0;
-    private float destY = 0;
-    private Point location = new Point(0,0);
-    private float speed = 60; //how fast this moves.
-    private float dx;
-    private float dy;
-    
-    
-    public Bullet(int startX, int startY, float destX, float destY)
-    {
-       this.startX = startX;
-       this.startY = startY;
-       location.setLocation(startX, startY);
-       this.destX = destX;
-       this.destY = destY;
-       recalculateVector(destX, destY);
+	private int startX;								// The starting x-coordinate of the bullet.
+	private int startY;								// The starting y-coordinate of the bullet.
+	private float destX;							// The destination x-coordinate of the bullet.
+	private float destY;							// The destination y-coordinate of the bullet.
+	private Point location;							// The bullet's location.
+	private float speed;							// How fast the bullet moves.
+	private float dx;								// The offset for the x-coordinate each frame.
+	private float dy;								// The offset for the y-coordinate each frame.
 
-    }
-    
-    /**
-     * Calculates a new vector based on the input destination X and Y.
-     * @param destX
-     * @param destY
-     */
-    public void recalculateVector(float destX, float destY){
-       float rad = (float)(Math.atan2(destX - startX, startY - destY));
-       
-       //Can set different speeds here, if you wanted.
-       speed = 60;
-       
-       this.dx = (float) Math.sin(rad) * speed;
-       this.dy = -(float) Math.cos(rad) * speed;
-    }
-    
-    /**
-     * Recalculates the vector for this bullet based on the current destination.
-     */
-    public void recalculateVector()
-    {
-       recalculateVector(destX, destY);
-    }
-    
-    /**
-     * Moves this bullet.
-     */
-    public void move()
-    {
-       float x = (float) location.getX();
-       float y = (float) location.getY();
-       
-       x += dx;
-       y += dy;
-       
-       location.setLocation(x, y);
-    }
-    
-    public float returnX(){
-    	return (float)location.getX();
-    }
-    
-    public float returnY(){
-    	return (float)location.getY();
-    }
+
+	public Bullet(int startX, int startY, float destX, float destY)
+	{
+		// Initialises variables.
+		location = new Point(0,0);
+		this.startX = startX;
+		this.startY = startY;
+		location.setLocation(startX, startY);
+		this.destX = destX;
+		this.destY = destY;
+		
+		// Calculates where the bullet needs to go.
+		recalculateVector(destX, destY);
+		
+		// Sets the bullet's speed.
+		speed = 60;
+	}
+
+	/**
+	 * Calculates the vector in which the bullet must travel to hit the target.
+	 * 
+	 * @param destX The x-coordinate of the enemy.
+	 * @param destY The y-coordinate of the enemy.
+	 */
+	public void recalculateVector(float destX, float destY){
+		// Calculates the angle that the bullet must travel in radians.
+		float rad = (float)(Math.atan2(destX - startX, startY - destY));
+
+		// Calculates the change in x and y based on the angle.
+		this.dx = (float) Math.sin(rad) * speed;
+		this.dy = -(float) Math.cos(rad) * speed;
+	}
+
+	/**
+	 * Moves the bullet.
+	 */
+	public void move()
+	{
+		// Gets the bullets current location.
+		float x = (float) location.getX();
+		float y = (float) location.getY();
+
+		// Sets the new location based on the dx and dy calculated in recalculateVector().
+		x += dx;
+		y += dy;
+
+		// Updates the location of the bullet.
+		location.setLocation(x, y);
+	}
+
+	/**
+	 * Gets the bullet's x-coordinate.
+	 * 
+	 * @return The bullet's x-coordinate.
+	 */
+	public float returnX(){
+		return (float)location.getX();
+	}
+
+	/**
+	 * Gets the bullet's y-coordinate.
+	 * 
+	 * @return The bullet's y-coordinate.
+	 */
+	public float returnY(){
+		return (float)location.getY();
+	}
 
 }
--- src/entities/WordList.java
+++ src/entities/WordList.java
@@ -12,11 +12,14 @@ import controllers.Settings;
 
 public class WordList implements Serializable {
 
-	public static HashMap<Integer, String> dic;
-	private Path path;
+	public static HashMap<Integer, String> dic;			// A HashMap of all of the words in the generated dictionary.
+	private Path path;									// A Path to use to locate files.
 	
 	public WordList() throws IOException {}
 
+	/**
+	 * Generates a new dictionary HashMap based on the current level.
+	 */
 	public void populateList() { 
 		dic = new HashMap<>();
 		
@@ -28,15 +31,12 @@ public class WordList implements Serializable {
 		}
 		else if(Settings.level == 3){
 			path = Paths.get("G:/UniversityWork/Software Design and Development/Modelling/Eclipse files/KMD/res/dic/threeLetterWords.txt");
-
 		}
 		else if(Settings.level == 4){
 			path = Paths.get("G:/UniversityWork/Software Design and Development/Modelling/Eclipse files/KMD/res/dic/fourLetterWords.txt");
-
 		}
 		else if(Settings.level == 5){
 			path = Paths.get("G:/UniversityWork/Software Design and Development/Modelling/Eclipse files/KMD/res/dic/fiveLetterWords.txt");
-
 		}
 		else if(Settings.level == 6){
 			path = Paths.get("G:/UniversityWork/Software Design and Development/Modelling/Eclipse files/KMD/res/dic/sixLetterWords.txt");
@@ -45,6 +45,7 @@ public class WordList implements Serializable {
 			path = Paths.get("G:/UniversityWork/Software Design and Development/Modelling/Eclipse files/KMD/res/dic/noLimitWords.txt");
 		}
 		
+		// If there is a path.
 		if (path != null){
 			// This scans through the word file and for each line it adds a new entry to the HashMap and gives it a sequential number.
 		    try (Scanner scanner =  new Scanner(path)){
@@ -62,7 +63,9 @@ public class WordList implements Serializable {
 	}
 
 
-	// Prints the contents of the dictionary.  This was initially for testing purposes.
+	/**
+	 *  Prints the contents of the dictionary.  This was for testing purposes.
+	 */
 	public void dumpDic(){
 		Integer i = 0;
 		while (dic.containsKey(i)){
@@ -71,7 +74,11 @@ public class WordList implements Serializable {
 		}
 	}
 	
-	// Pulls a random word from the current dictionary.
+	/**
+	 * Pulls a random word from the current dictionary.
+	 * 
+	 * @return A random word from the dictionary.
+	 */
 	public String getWord(){
 		RandomLocation rand = new RandomLocation();
 		Integer i = rand.getRand(dic.size()-1);
@@ -80,6 +87,11 @@ public class WordList implements Serializable {
 		return randWord;
 	}
 	
+	/**
+	 * The dictionaries size.
+	 * 
+	 * @return The number of words in the dictionary.
+	 */
 	public int size(){
 		return dic.size();
 	}
